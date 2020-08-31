f9f32cae1e4a9a0a31443cb721997d9b9e3ed63c
2009miles <bruno.m.gomes.95@gmail.com>
2020-09-01 00:06:44 +0100

    Project Compete

--- JS/Shopping-cart/addOrRemove.js
+++ JS/Shopping-cart/addOrRemove.js
@@ -1,11 +1,16 @@
-// let add = document.querySelectorAll('.qtyAdd');
-// let remove = document.querySelectorAll('.qtyRemove');
-// let valueHolder = document.querySelectorAll('.currentQty');
+let add = document.querySelectorAll('.qtyAdd');
+let remove = document.querySelectorAll('.qtyRemove');
 
-// for(i=0;i<add.length;i++){
-//     test = i;
-//     add[i].addEventListener('click', function(e){
-//         this.parentElement.parentElement.innerText.replace(this.parentElement.parentElement.innerText, Number(this.parentElement.parentElement.innerText) + 1)
-//     });
-// }
+add.forEach(add => add.addEventListener('click', function(e){
+    currentValue = Number(add.parentElement.parentElement.querySelector('.currentQty').innerText);
+    add.parentElement.parentElement.querySelector('.currentQty').innerText = `${currentValue + 1}`;
+}));
 
+remove.forEach(remove => remove.addEventListener('click', function(e){
+    currentValue = Number(remove.parentElement.parentElement.querySelector('.currentQty').innerText);
+    if(currentValue===1){
+        remove.parentElement.parentElement.parentElement.parentElement.innerHTML = '';
+    }else{
+        remove.parentElement.parentElement.querySelector('.currentQty').innerText = `${currentValue - 1}`;
+    }
+}));
--- JS/Shopping-cart/formsSubmission.js
+++ JS/Shopping-cart/formsSubmission.js
@@ -1,9 +1,69 @@
+function submitForm(event){
+    event.preventDefault()
+    let formValues = [];
+    let shippingSelection = document.querySelector('.shippingDeliveryMethod');
+    let shippingOption = shippingSelection.options[shippingSelection.selectedIndex].text;
+    formValues.push(["shippingDeliveryMethod",shippingOption]);
 
-let submissionButton = document.querySelector('.orderConfirmationInput');
+    formOneInputs = document.querySelector('.shippingInformationForm').querySelectorAll('input');
+    for(i=0; i<formOneInputs.length; i++){
+        let name = formOneInputs[i].name;
+        let value = formOneInputs[i].value;
+        formValues.push([name,value]);
+    }
+    let paymentSelection = document.querySelector('.paymentMethod');
+    let paymentOption = paymentSelection.options[paymentSelection.selectedIndex].text;
+    formValues.push(["paymentMethod",paymentOption]);
 
-submissionButton.addEventListener('click', ()=>{
-    document.querySelector(".shippingInformationForm").submit();
-    document.querySelector(".paymentInformationForm").submit();
-})
+    
 
-document.onsubmit(alert('form submitted'));
+    if(formValidation() == true){
+        document.querySelector('.orderConfirmationInput').classList.add('inactive');
+        document.querySelector('#formFeedbackWarning').classList.add('inactive');
+        document.querySelector('#formFeedback').classList.remove('inactive');
+    }else{
+        document.querySelector('#formFeedbackWarning').classList.remove('inactive');
+    }
+}
+
+function formValidation(){
+    let validinputs = 0;
+    let shippingSelection = document.querySelector('.shippingDeliveryMethod');
+    if(shippingSelection.options[shippingSelection.selectedIndex].text == "Select delivery method"){
+        shippingSelection.classList.add('invalid');
+    }else{
+        shippingSelection.classList.remove('invalid');
+        validinputs++;
+    }
+
+    formOneInputs = document.querySelector('.shippingInformationForm').querySelectorAll('input');
+    for(j=0; j<formOneInputs.length; j++){
+        if(formOneInputs[j].value.length == 0 && formOneInputs[j].className.includes('required')){
+            formOneInputs[j].classList.add('invalid');
+        }else{
+            formOneInputs[j].classList.remove('invalid');
+            validinputs++;
+        }
+    }
+
+    email = document.querySelector('.shippingEmail');
+    if(!email.value.includes('@')){
+        email.classList.add('invalid');
+    }else{
+        email.classList.remove('invalid');
+    }
+
+    let paymentSelection = document.querySelector('.paymentMethod');
+    if(paymentSelection.options[paymentSelection.selectedIndex].text == "Select payment method"){
+        paymentSelection.classList.add('invalid');
+    }else{
+        paymentSelection.classList.remove('invalid');
+        validinputs++;
+    }
+
+    if(validinputs==11){
+        return true
+    }else{
+        return false
+    }
+}
